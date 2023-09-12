17effe326de161f17229fbd2f8fe1dbee83f7a3f
BuildTools <unconfigured@null.spigotmc.org>
2023-09-11 22:03:23 -0300

    first commit

--- teste.py
+++ teste.py
@@ -2,5 +2,5 @@ nome = range(1, 10)
 for i, valor in enumerate(nome):
     print(f'Chave {i} e Valor {valor}')
     if input("Parar?") == "s":
-        print("Ok, cancelando..")
+        print("Cancelando..")
         break
